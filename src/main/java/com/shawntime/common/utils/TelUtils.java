11b773d1c2b42f429858099271a8728b93638860
mark.dong <234498142@qq.com>
2021-06-04 02:17:42 +0800

    tel rf trim

--- src/main/java/com/shawntime/common/utils/TelUtils.java
+++ src/main/java/com/shawntime/common/utils/TelUtils.java
@@ -9,17 +9,14 @@ public final class TelUtils {
 
     private TelUtils() {
     }
-
     public static boolean isValidMobiePhoneNum(String telNumber) {
         final String regexMobile = "^1\\d{10}$";
         return Pattern.matches(regexMobile, telNumber);
     }
-
     public static boolean isValidFixPhoneNum(String telNumber) {
         final String regexPhone = "^((0\\d{2,3})-)(\\d{7,8})(-(\\d{3,}))?$";
         return Pattern.matches(regexPhone, telNumber);
     }
-
     public static boolean isMayBeMobiePhoneNum(String telNumber) {
         final String regexMobile = "^\\d{11}$";
         return Pattern.matches(regexMobile, telNumber);
